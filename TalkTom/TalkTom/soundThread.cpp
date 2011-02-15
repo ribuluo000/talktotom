f489b2ff0747a9ad5a2fc941d20b431da3cc573b
aicrosoft1104 <aicrosoft1104@61e0f6aa-79d2-64b1-0467-50e2521aa597>
2011-02-15 11:36:39 +0000

    没有采用主线程消息情况下的版本

--- TalkTom/TalkTom/soundThread.cpp
+++ TalkTom/TalkTom/soundThread.cpp
@@ -24,8 +24,11 @@ unsigned __stdcall soundThread(void * p)
 	// wait until we get the info of the marker
 	WaitForSingleObject(g_global.hWaitForMarker, INFINITE);
 
-	HWND hWnd = FindWindow(NULL, L"");
-	SpeechToText._NotifyWindowMessage(hWnd, MS_MYMESSAGE);
+	HWND hWnd = FindWindow(NULL, L"CamShift Adjust Window");
+	SetWindowText(hWnd, L"asdf");
+
+	bool b = false;
+	b = SpeechToText._NotifyWindowMessage(hWnd, MS_MYMESSAGE);
 	SpeechToText._RegisterMessageFunc(SPEI_SOUND_START, FuncSoundStart, NULL);
 	SpeechToText._RegisterMessageFunc(SPEI_RECOGNITION, FuncFind , NULL);
 
@@ -37,8 +40,10 @@ unsigned __stdcall soundThread(void * p)
 		WaitForSingleObject(g_global.hWaitForMarker, INFINITE);
 
 		// we can mix the sound system now
-		GetMessage(&msg, hWnd, 0, 0);
-		
+		GetMessage(&msg, hWnd, 0, 0x1000);
+
+		printf("deal message!\n");
+
 		SpeechToText._DealMessage(msg.message);
 
 		ResetEvent(g_global.hWaitForMarker);
